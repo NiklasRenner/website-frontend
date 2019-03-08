ada233c4049261a65f0bb44eb831387a10f46c44
Niklas Renner Nielsen <NiklasRennerNielsen@gmail.com>
2019-03-08 04:06:28 +0100

    removed textarea.

--- src/app/components/paste-show/paste-show.component.ts
+++ src/app/components/paste-show/paste-show.component.ts
@@ -9,26 +9,9 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class PasteShowComponent implements OnInit {
   content = '';
-  contentLength = 0;
   id = null;
 
   constructor(private apiService: ApiService, private route: ActivatedRoute) {
-  }
-
-  rows(): number {
-    return this.contentLength > 20 ? this.contentLength : 20;
-  }
-
-  private countLines(str: string): number {
-    let result = 0;
-
-    for (let i = 0; i < str.length; i++) {
-      if (str.charAt(i) === '\n') {
-        result++;
-      }
-    }
-
-    return result;
   }
 
   ngOnInit() {
@@ -36,7 +19,6 @@ export class PasteShowComponent implements OnInit {
       this.id = params.id;
       this.apiService.getPaste(this.id).subscribe(data => {
         this.content = data;
-        this.contentLength = this.countLines(this.content);
       });
     });
   }
